2302e766c379daa605d7687374e3c0781773c197
rizhul.januar <rayjam66@gmail.com>
2026-09-14 22:03:12 +0700

    chore: create Laravel project

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok',
+        'application' => config('app.name')
+    ]);
+});
